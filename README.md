e8fbc202d0c1759653d2f1da46a2a539a61855d1
Danilo Cunha <danilokage@gmail.com>
2017-07-27 16:29:35 -0300

    Add basic functionalities to bot.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # basic_telegram_bot
-A basic Telegram bot in Python that echoes whatever is sent to it.
+
+To make the basic bot work it is needed to create a new ".py" file called "api_key.py", containing a variable called "TOKEN" which value holds the API_KEY string given by Telegram's the BotFather.
---
+++ basic_bot.py
@@ -0,0 +1,76 @@
+from api_key import TOKEN
+import time
+import json
+import requests
+import urllib.parse
+
+URL = "https://api.telegram.org/bot{}/".format(TOKEN)
+
+
+# Do the request to the url and get content
+def get_url(url):
+    response = requests.get(url)
+    content = response.content.decode("utf8")
+    return content
+
+
+# Calls get_url, receives returned content and returns the json
+def get_json_from_url(url):
+    content = get_url(url)
+    js = json.loads(content)
+    return js
+
+
+# Calls get_json_from_url from the getUpdates url
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
+    js = get_json_from_url(url)
+    return js
+
+
+# Checks number of updates,
+def get_last_chat_id_and_text(updates):
+    num_updates = len(updates["result"])
+    last_update = num_updates - 1
+    text = updates["result"][last_update]["message"]["text"]
+    chat_id = updates["result"][last_update]["message"]["chat"]["id"]
+    return text, chat_id
+
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def send_message(text, chat_id):
+    text = urllib.parse.quote_plus(text)
+    url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
+    get_url(url)
+
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
